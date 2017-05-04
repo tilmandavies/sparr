6e7c7a4eefd5a45065b7cb5e96bd2e735e0b3cd9
Tilman Davies <tdavies@Syss-MBP.fritz.box>
2017-05-04 20:18:53 +1200

    range function tweak

--- R/checkran.R
+++ R/checkran.R
@@ -7,11 +7,11 @@ checkran <- function(ran,nm){
   return(ran)
 }
 
-checkranin <- function(ran,vals,nm1,nm2){
+checkranin <- function(ran,vals,nm1){
   if(!is.vector(ran)) stop(paste("'",nm1,"' must be a vector",sep=""))
   if(!is.numeric(ran)) stop(paste("'",nm1,"' must be numeric",sep=""))
   if(length(ran)!=2) stop(paste("'",nm1,"' must have length 2",sep=""))
   if(ran[2]<=ran[1]) stop(paste("'",nm1,"[1]' must be < '",nm1,"[2]'",sep=""))
-  if(any(vals<ran[1])||any(vals>ran[2])) stop(paste("all '",nm2,"' values must lie within interval '",nm1,"'",sep=""))
+  if(any(vals<ran[1])||any(vals>ran[2])) stop(paste("all time values must lie within interval '",nm1,"'",sep=""))
   return(ran)
 }
