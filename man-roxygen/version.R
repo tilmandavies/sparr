b413dd18d8a09c40456aaf6b29202dd0cdd8feec
Jonathan Marshall <jmarshall@never.you.mind>
2017-05-11 11:11:42 +1200

    auto-generate the documentation version and date using roxygen templates

---
+++ man-roxygen/version.R
@@ -0,0 +1,7 @@
+#' @details
+#' \tabular{ll}{
+#' Package: \tab sparr\cr
+#' Date:\tab <%= packageDescription('sparr', lib.loc='..')$Date %>\cr
+#' Version:\tab <%= packageDescription('sparr', lib.loc='..')$Version %>\cr
+#' License: \tab GPL (>= 2)\cr
+#' }
