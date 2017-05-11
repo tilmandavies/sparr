61c5238ad17b26cf7cebae6a6e119bc0c31f440e
Tilman Davies <tdavies@Syss-MacBook-Pro.local>
2017-05-11 22:12:07 +1200

    fft2d docs corrected

--- R/fft.R
+++ R/fft.R
@@ -5,15 +5,14 @@
 #' 
 #' This function is called wherever \code{sparr} seeks to perform a 2D fast-Fourier
 #' transform. Where available, computational expense is noticeably reduced by appealing to routines 
-#' in the independent `FFTW' toolbox. The user is encouraged to install the `FFTW' tools 
-#' from \url{http://www.fftw.org} and the corresponding R package \code{fftwtools} from CRAN;
+#' in the independent `FFTW' toolbox. The user is encouraged to install the corresponding R package \code{fftwtools} from CRAN;
 #' this function will automatically detect and use the faster option, otherwise will 
-#' defers to the built-in \code{\link[stats]{fft}}.
+#' defer to the built-in \code{\link[stats]{fft}}.
 #' 
 #' 
 #' @param x A numeric matrix to be transformed.
 #' @param inverse Whether it should compute the inverse transform (defaults to \code{FALSE}).
-#' @param fftw Whether the `FFTW' tools and the \code{fftwtools} R package are available.
+#' @param fftw Whether the \code{fftwtools} R package is available.
 #' @return The fast-Fourier (inverse) transform. A complex-valued matrix of the same size as \code{x}.
 #' 
 #' @author J.C. Marshall
@@ -37,6 +36,13 @@ fft2d <- function(x, inverse=FALSE, fftw = sparr:::fftw_available()) {
 }
 
 fftw_available <- function() {
-  yeahnah <- (requireNamespace("fftwtools", quietly=TRUE) && (packageVersion("fftwtools") >= "0.9-8"))
+  yeahnah <- requireNamespace("fftwtools", quietly=TRUE)
+  if(yeahnah) yeahnah <- packageVersion("fftwtools") >= "0.9-8"
   return(yeahnah)
 }
+
+# The user is encouraged to install the `FFTW' tools 
+# from \url{http://www.fftw.org} and the corresponding R package \code{fftwtools} from CRAN;
+# this function will automatically detect and use the faster option, otherwise will 
+# defers to the built-in \code{\link[stats]{fft}}.
+#
