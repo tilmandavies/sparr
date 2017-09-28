c4779141067eb78b12db10bc6c9659855cb82de8
Tilman Davies <tdavies@Syss-MBP.fritz.box>
2017-09-28 22:31:06 +1300

    tiny administrative tweaks for CRAN and spatstat

--- R/zzz.R
+++ R/zzz.R
@@ -4,5 +4,5 @@
 	s3 <- "  \\__ \\/ ___/ __  /  ___/  ___/        \n"
 	s4 <- " ___/ / /  / / / / /\\ \\/ /\\ \\          \n"
 	s5 <- paste0("/____/_/  /_/ /_/_/  \\__/  \\_\\   v", packageDescription("sparr")$Version, "\n\n")
-	packageStartupMessage(paste("\n\nWelcome to\n",s1,s2,s3,s4,s5,"***NB: MAJOR CHANGES FROM PREVIOUS VERSIONS***\n- type news(package=\"sparr\") for an overview\n- type help(\"sparr\") for documentation\n- type citation(\"sparr\") for how to cite\n",sep=""),appendLF=TRUE)
+	packageStartupMessage(paste("\n\nWelcome to\n",s1,s2,s3,s4,s5,"***NB: MAJOR CHANGES FROM PREVIOUS VERSIONS < 2.1-10***\n- type news(package=\"sparr\") for an overview\n- type help(\"sparr\") for documentation\n- type citation(\"sparr\") for how to cite\n",sep=""),appendLF=TRUE)
 } #\n*type vignette(\"sparr2\") to access the accompanying article [unimplemented]\n*type citation(\"sparr2\") for how to cite use of this package\n"
