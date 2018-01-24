4c3e32f60955bba95b4daaa6c41d1776e863f5c3
tilmandavies <tdavies@maths.otago.ac.nz>
2018-01-24 16:08:49 +1300

    bug squash

--- R/LSCV.density.spatial.single.R
+++ R/LSCV.density.spatial.single.R
@@ -1,6 +1,6 @@
 LSCV.density.spatial.single <- function(h,pp,res,edge,za){
   if(h<=0) return(NA)
-  temp.dens <- density(pp,h,edge=edge,dimyx=res,positive=TRUE,diggle=FALSE)
+  temp.dens <- density(pp,h,edge=edge,dimyx=res,positive=FALSE,diggle=FALSE)
   temp.int <- integral(temp.dens)
   temp.dens.pts <- density(pp,sigma=h,edge=edge,dimyx=res,at="points",positive=FALSE,leaveoneout=TRUE,diggle=FALSE)/temp.int
   temp.dens <- temp.dens/temp.int
