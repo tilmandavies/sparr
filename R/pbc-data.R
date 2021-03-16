c8037ec5226d4e6b9e31aa5f13e61231451a9ba3
tilmandavies <tdavies@maths.otago.ac.nz>
2021-03-16 22:33:29 +1300

    pedantic CRAN link reformatting

--- R/pbc-data.R
+++ R/pbc-data.R
@@ -13,8 +13,7 @@
 #' Survey Coordinates (km).
 #' @docType data
 #' @keywords data
-#' @section Acknowledgements: The authors thank Prof. Peter Diggle at Lancaster
-#' University (\url{https://www.lancs.ac.uk/staff/diggle/}) for providing access
+#' @section Acknowledgements: The authors thank Prof. Peter Diggle for providing access
 #' to these data.
 #' @references Davies, T.M. and Hazelton, M.L. (2010), Adaptive kernel
 #' estimation of spatial relative risk, \emph{Statistics in Medicine},
